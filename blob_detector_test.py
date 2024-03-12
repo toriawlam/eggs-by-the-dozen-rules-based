8b10868fa85e78320afc45d48d26fd6f27517853
lamv13 <toriawlam@gmail.com>
2024-03-12 14:49:23 -0400

    updated code w semi-correct params

--- blob_detector_test.py
+++ blob_detector_test.py
@@ -4,9 +4,33 @@ import numpy as np;
  
 # Read image
 im = cv2.imread("strongylid_test_2.jpg", cv2.IMREAD_GRAYSCALE)
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+ 
+# Change thresholds
+params.minThreshold = 175
+params.maxThreshold = 200
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 3.14159 * 20 * 20
+
+# Filter by Circularity
+params.filterByCircularity = False
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = False
+params.minConvexity = 0.87
+ 
+# Filter by Inertia
+params.filterByInertia = False
+params.minInertiaRatio = 0.60 #.64
+params.maxInertiaRatio = 0.75 #.7
  
 # Set up the detector with default parameters.
-detector = cv2.SimpleBlobDetector_create()
+detector = cv2.SimpleBlobDetector_create(params)
  
 # Detect blobs.
 keypoints = detector.detect(im)
@@ -18,33 +42,3 @@ im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
-
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
- 
-# Change thresholds
-params.minThreshold = 10
-params.maxThreshold = 200
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 1500
- 
-# Filter by Circularity
-params.filterByCircularity = False
-params.minCircularity = 0.1
- 
-# Filter by Convexity
-params.filterByConvexity = False
-params.minConvexity = 0.87
- 
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.01
-
-# Create a detector with the parameters
-ver = (cv2.__version__).split('.')
-if int(ver[0]) < 3 :
-  detector = cv2.SimpleBlobDetector_create(params)
-else : 
-  detector = cv2.SimpleBlobDetector_create(params)

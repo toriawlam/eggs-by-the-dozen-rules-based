8310ebafd448e58fc72bedba59f96d4e8c3f9ac9
lamv13 <toriawlam@gmail.com>
2024-03-12 14:49:35 -0400

    updated code w/ params

--- blob_detector_test.py
+++ blob_detector_test.py
@@ -1,20 +1,24 @@
 # Standard imports
 import cv2
 import numpy as np;
- 
+
 # Read image
 im = cv2.imread("strongylid_test_2.jpg", cv2.IMREAD_GRAYSCALE)
+# gray= cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
- 
+
 # Change thresholds
-params.minThreshold = 175
+params.minThreshold = 10
 params.maxThreshold = 200
+
+# Filter by Color
+params.filterByColor = True
 
 # Filter by Area.
 params.filterByArea = True
-params.minArea = 3.14159 * 20 * 20
+params.minArea = 1500
 
 # Filter by Circularity
 params.filterByCircularity = False
@@ -23,22 +27,21 @@ params.minCircularity = 0.1
 # Filter by Convexity
 params.filterByConvexity = False
 params.minConvexity = 0.87
- 
+
 # Filter by Inertia
-params.filterByInertia = False
-params.minInertiaRatio = 0.60 #.64
-params.maxInertiaRatio = 0.75 #.7
- 
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
 # Set up the detector with default parameters.
 detector = cv2.SimpleBlobDetector_create(params)
- 
+
 # Detect blobs.
 keypoints = detector.detect(im)
- 
+
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
- 
+
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
